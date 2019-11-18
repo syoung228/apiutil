088f9d44ec2379d61772cd91b9e56f934028fd29
syoung <770994982@qq.com>
2019-11-18 16:19:32 +0800

    推广 详情

--- src/main/java/com/WeApiConstats.java
+++ src/main/java/com/WeApiConstats.java
@@ -13,5 +13,10 @@ package com;/**
  */
 
 public class WeApiConstats {
+    /** 物料列表*/
     public static final String ADS = "https://weapi.info/api/v1/ads";
+    /** 物料详情*/
+    public static final String ADS_INFO = "https://weapi.info/api/v1/ads/";
+    /** 推广短信*/
+    public static final String SMS = "https://weapi.info/api/v1/sms";
 }
