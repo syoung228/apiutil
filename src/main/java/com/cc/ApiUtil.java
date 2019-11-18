376e7bab4b24f942042f221f63fd0b856ff5d821
syoung <770994982@qq.com>
2019-11-18 17:19:07 +0800

    异常捕获问题

--- src/main/java/com/cc/ApiUtil.java
+++ src/main/java/com/cc/ApiUtil.java
@@ -16,8 +16,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @ClassName
  * @author songy
+ * @ClassName
  * @Date
  * @Version 1.0
  */
@@ -25,56 +25,62 @@ import java.util.Map;
 public class ApiUtil {
     /**
      * 获取物料列表
+     *
      * @param per_page 每条页数
-     * @param page 当前页
+     * @param page     当前页
      * @return
+     * @throws ApiException
      */
-    public static Data getAds(String per_page,String page) throws ApiException{
+    public static Data getAds(String per_page, String page) throws ApiException {
         Data data = null;
-        if(page == null){
+        if (page == null) {
             page = "1";
         }
-        String url = WeApiConstats.ADS+"?page="+page;
-        if(per_page != null){
-            url = url + "&per_page=" +per_page;
+        String url = WeApiConstats.ADS + "?page=" + page;
+        if (per_page != null) {
+            url = url + "&per_page=" + per_page;
         }
         String result = HttpClientUtil.httpGet(url);
-        JSONObject jsonObject  = JSONObject.parseObject(result);
-        if(!jsonObject.get("code").toString().startsWith("2")){
-            JSONObject jsonObject2  = JSONObject.parseObject(jsonObject.get("error").toString());
+        JSONObject jsonObject = JSONObject.parseObject(result);
+        if (!jsonObject.get("code").toString().startsWith("2")) {
+            JSONObject jsonObject2 = JSONObject.parseObject(jsonObject.get("error").toString());
             throw new ApiException(jsonObject2.get("message").toString());
         }
-        data = JSONObject.toJavaObject(jsonObject,Data.class);
+        data = JSONObject.toJavaObject(jsonObject, Data.class);
         return data;
     }
 
     /**
      * 物料商品详情
+     *
      * @param adsId 物料商品id
      * @return
+     * @throws ApiException
      */
     public static Data getAdsInfo(String adsId) throws ApiException {
         Data data = new Data();
-        String url = WeApiConstats.ADS_INFO+adsId;
+        String url = WeApiConstats.ADS_INFO + adsId;
         String result = HttpClientUtil.httpGet(url);
-        JSONObject jsonObject  = JSONObject.parseObject(result);
-        if(!jsonObject.get("code").toString().startsWith("2")){
-            JSONObject jsonObject2  = JSONObject.parseObject(jsonObject.get("error").toString());
+        JSONObject jsonObject = JSONObject.parseObject(result);
+        if (!jsonObject.get("code").toString().startsWith("2")) {
+            JSONObject jsonObject2 = JSONObject.parseObject(jsonObject.get("error").toString());
             throw new ApiException(jsonObject2.get("message").toString());
         }
-        data = JSONObject.toJavaObject(jsonObject,Data.class);
+        data = JSONObject.toJavaObject(jsonObject, Data.class);
         return data;
 
     }
 
     /**
      * 短信推广
+     *
      * @param api_key 客户认证
-     * @param phone 电话号码
-     * @param adsId 物料id
+     * @param phone   电话号码
+     * @param adsId   物料id
      * @return 短信id
+     * @throws ApiException
      */
-    public static String sms(String api_key,String phone,String adsId) throws ApiException{
+    public static String sms(String api_key, String phone, String adsId) throws ApiException {
         String smsId = null;
         Map<String, Object> map = new HashMap<>();
         map.put("phone", phone);
